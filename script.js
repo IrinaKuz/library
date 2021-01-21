e26fe34d3da791a87e84838a45ef16dd336753ab
IrinaKuz <ira.kuznets@gmail.com>
2021-01-21 00:03:30 -0500

    Added a form, added style for the form in style.css

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let library = [];
 
-function Book() {
-
+function Book(name, author, genre, pages, status) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(book) {
+    library.push(book);
 }
